6f7193c323ad7ff7f97bab3e1451f0d82b2d5724
sbbs8668 <sbbs8668@gmail.com>
2022-06-15 11:26:12 +0300

    hw#1

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
-###
+#### HW0 START
+CONST TEST_CONSTANTA = 'asd';
+$user_name = "Igor";
+
+function show_something(string $something = ''): string
+{
+    if ($something) {
+        echo $something;
+    }
+    return 'SOMETHING';
+}
+
+$a = 1;
+$b = '1';
+
+if ($a == $b) {
+    echo 'hi';
+    show_something(TEST_CONSTANTA);
+}
+#### HW0 END
+
