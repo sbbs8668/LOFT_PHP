9b027045088a89e89515a95f7051ca308ac1c168
sbbs8668 <sbbs8668@gmail.com>
2022-06-28 06:49:44 +0300

    init ok

---
+++ hw3/app/Controller/User.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+
+class User
+{
+  public function method()
+  {
+
+  }
+}
---
+++ hw3/html/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use App\Controller\User;
+
+$userController = new User();
